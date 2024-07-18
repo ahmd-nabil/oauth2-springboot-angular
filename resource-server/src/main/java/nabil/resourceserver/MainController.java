db71b3fd73a877c87ab79de365d840ba5cb8045e
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-07-18 19:37:43 +0300

    Customizing scope prefix

--- resource-server/src/main/java/nabil/resourceserver/MainController.java
+++ resource-server/src/main/java/nabil/resourceserver/MainController.java
@@ -1,6 +1,7 @@
 package nabil.resourceserver;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping("/main") // just authenticated
-    public String main() {
+    public String main(Authentication authentication) {
+        System.out.println(authentication.getAuthorities());
         return "Hello World!";
     }
+
+    @PreAuthorize("hasAuthority('user')")
+    @GetMapping("/user")
+    public String user() {
+        return "Hello User!";
+    }
+
+    @PreAuthorize("hasAuthority('admin')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin!";
+    }
+
 }
